77a6591000fb064ca32d6aecc8517ba36c5d1d9e
Jake Velicer <007velicer@gmail.com>
2019-06-02 18:12:23 -0500

    Car Beginning Test

---
+++ Assets/Scripts/CarBehaviorTest.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBehaviorTest : MonoBehaviour
+{
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float steeringAngleInput;
+
+    public WheelCollider frontDriverW, frontPassengerW;
+    public WheelCollider rearDriverW, rearPassengerW;
+    public Transform frontDriverT, frontPassengerT;
+    public Transform rearDriverT, rearPassengerT;
+    public float maxSteerAngle;
+    public float motorForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate() {
+
+        GetInput();
+        Steer();
+        Accelerate();
+        UpdateWheelPoses();
+    }
+
+    public void GetInput() {
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    public void Steer() {
+
+        steeringAngleInput = maxSteerAngle * horizontalInput;
+        frontDriverW.steerAngle = steeringAngleInput;
+        frontPassengerW.steerAngle = steeringAngleInput;
+    }
+
+    public void Accelerate() {
+
+        frontDriverW.motorTorque = verticalInput * motorForce;
+        frontPassengerW.motorTorque = verticalInput * motorForce;
+    }
+
+    public void UpdateWheelPoses() {
+
+        UpdateWheelPose(frontDriverW, frontDriverT);
+        UpdateWheelPose(frontPassengerW, frontPassengerT);
+        UpdateWheelPose(rearDriverW, rearDriverT);
+        UpdateWheelPose(rearPassengerW, rearPassengerT);
+    }
+
+    public void UpdateWheelPose(WheelCollider collider, Transform transform) {
+
+        Vector3 pos = transform.position;
+        Quaternion quat = transform.rotation;
+
+        collider.GetWorldPose(out pos, out quat);
+
+        transform.position = pos;
+        transform.rotation = quat;
+    }
+}
---
+++ Assets/Scripts/SimpleCameraFollow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCameraFollow : MonoBehaviour
+{
+
+    public Transform objectToFollow;
+    public Vector3 offest;
+    public float followSpeed;
+    public float lookSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        LookAtTarget();
+        MoveToTarget();
+    }
+
+    public void LookAtTarget() {
+
+        Vector3 lookDirection = objectToFollow.position - transform.position;
+        Quaternion rot = Quaternion.LookRotation(lookDirection, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, lookSpeed * Time.deltaTime);
+    }
+
+    public void MoveToTarget() {
+
+        Vector3 targetPos = objectToFollow.position +
+                            objectToFollow.forward * offest.z +
+                            objectToFollow.right * offest.x +
+                            objectToFollow.up * offest.y;
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, followSpeed * Time.deltaTime);
+    }
+}
