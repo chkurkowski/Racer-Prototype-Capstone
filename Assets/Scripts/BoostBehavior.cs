3cb309de6221e50d1b6533a859630e2e62bb02e1
Jake Velicer <007velicer@gmail.com>
2019-06-30 18:04:50 -0500

    Terrain added, Camera Exaggeration, Commented Code

--- Assets/Scripts/BoostBehavior.cs
+++ Assets/Scripts/BoostBehavior.cs
@@ -12,12 +12,15 @@ public class BoostBehavior : MonoBehaviour
     private float iniSpeed;
     private bool canBoost;
     private CarPhysicsBehavior carScript;
+    private SimpleCameraFollow mainCamera;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Sets variables for gameplay
         canBoost = true;
         carScript = GetComponent<CarPhysicsBehavior>();
+        mainCamera = GameObject.Find("Main Camera").GetComponent<SimpleCameraFollow>();
         iniSpeed = carScript.driveForce;
         boostCharge = iniSpeed + boostAmount;
     }
@@ -25,15 +28,11 @@ public class BoostBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Checks for Boost Input and calls corutine if possible
         if (Input.GetButtonDown("Fire2") && canBoost)
         {
             canBoost = false;
             StartCoroutine(Boost());
-        }
-
-        if (boostCharge < boostAmount && canBoost == true)
-        {
-            //boostCharge = (boostCharge + 0.05f) + Time.fixedDeltaTime;
         }
     }
     
@@ -41,12 +40,25 @@ public class BoostBehavior : MonoBehaviour
     {
         while (carScript.driveForce < boostCharge)
         {
+            // Lerps the camera follow speed to be slower
+            mainCamera.GetComponent<SimpleCameraFollow>().followSpeed =
+            Mathf.Lerp(10, mainCamera.followSpeed, Time.deltaTime);
+
+            // Adds speed to vehicles drive force over time
             carScript.driveForce += accelerationAdd;
             yield return new WaitForSeconds(0.05f);
         }
+
+        // Time while max boost is active
         yield return new WaitForSeconds(boostTime);
+
         while (carScript.driveForce > iniSpeed)
         {
+            // Lerps the camera follow speed to be faster
+            mainCamera.GetComponent<SimpleCameraFollow>().followSpeed =
+            Mathf.Lerp(mainCamera.followSpeed, 34, 6f * Time.deltaTime);
+
+            // Subtracts the vehicles drive force over time
             carScript.driveForce -= decelerationSub;
             yield return new WaitForSeconds(0.05f);
         }
