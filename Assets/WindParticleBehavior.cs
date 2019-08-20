ee28a7541f458e7ac79f05d75568a5f29574cb29
Jake Velicer <007velicer@gmail.com>
2019-08-19 22:40:14 -0500

    Particles tied to car and its speed

---
+++ Assets/WindParticleBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindParticleBehavior : MonoBehaviour
+{
+    public CarPhysicsBehavior theVehicleScript;
+    private ParticleSystem particles;
+    private float particleSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        particleSpeed = theVehicleScript.currentDriveForce / 2;
+        particles.startSpeed = particleSpeed;
+    }
+}
