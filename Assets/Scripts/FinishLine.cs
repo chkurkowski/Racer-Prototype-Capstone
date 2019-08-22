c04960da19641a36cf4ef06925df9dc7d96a18b7
Jake Velicer <007velicer@gmail.com>
2019-08-22 18:48:15 -0500

    Finish Line and Track Manager scripts

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    private TrackManager trackManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trackManager = GameObject.Find("Manager").GetComponent<TrackManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Vehicle"))
+        {
+            if (trackManager.checkIncrimate() == true)
+            {
+                trackManager.lap++;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TrackManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackManager : MonoBehaviour
+{
+    public int lap = 0;
+    private bool canIncrimate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool checkIncrimate()
+    {
+        return canIncrimate;
+    }
+}
