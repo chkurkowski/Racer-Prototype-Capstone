8be153c31469bbcee0f5590ef79567a2e6b8a19a
Jake Velicer <007velicer@gmail.com>
2019-08-22 21:32:23 -0500

    First Barrier Event Implemented

--- Assets/Scripts/BarrierEvent.cs
+++ Assets/Scripts/BarrierEvent.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class BarrierEvent : MonoBehaviour
 {
+    public GameObject[] objectsToDisable;
+    public GameObject[] particlesToEnable;
+
     public void BarrierEventActiavtion()
     {
-        
-    }
+        for (int i = 0; i < objectsToDisable.Length; i++)
+        {
+            objectsToDisable[i].SetActive(false);
+        }
+        for (int i = 0; i < particlesToEnable.Length; i++)
+        {
+            if (particlesToEnable[i] != null)
+            {
+                particlesToEnable[i].SetActive(true);
+            }
+        }
+
+    } 
 }
