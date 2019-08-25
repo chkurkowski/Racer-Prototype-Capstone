8caf671eb7398da91d1fa80c03e3ca0634a716f8
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2019-08-25 14:33:50 -0500

    Restructured the folders

---
+++ Assets/Scripts/Base Classes/CarHeatManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarHeatManager : MonoBehaviour
+{
+
+    public float heatCurrent = 100f;
+    public float heatStallLimit = 100f;
+    public float heatExplodeLimit = 120f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
