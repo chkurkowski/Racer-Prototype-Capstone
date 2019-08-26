69eb44c3354a1e4042ea808ede207fea3bbb843f
Jake Velicer <007velicer@gmail.com>
2019-08-25 20:29:29 -0500

    Oil fires affect heat

---
+++ Assets/Scripts/ObstacleDamage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleDamage : MonoBehaviour
+{
+    public float damage = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Vehicle"))
+        {
+            if (other.gameObject.GetComponent<CarHeatManager>() != null)
+            {
+                other.gameObject.GetComponent<CarHeatManager>().heatCurrent
+                = other.gameObject.GetComponent<CarHeatManager>().heatCurrent + damage;
+            }
+        }
+    }
+
+}
